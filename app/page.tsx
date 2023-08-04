63cca0a08e297946a97a95d20762cadc8f24bd6a
Abdurrahman Shofy Adianto <azophy@gmail.com>
2023-08-04 07:16:34 +0700

    experiment with advance encryp-decript case

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from 'react'
+const { split: shamirSplit, join: shamirJoin } = require('shamir');
 
 const sleep = async (milliseconds) => {
   await new Promise(resolve => {
@@ -8,7 +9,10 @@ const sleep = async (milliseconds) => {
 };
 
 export default function Home() {
-  const [key, setKey] = useState('')
+  const [status, setStatus] = useState('')
+  const [encryptionMessage, setEncryptionMessage] = useState('')
+  const [origMsg, setOrigMsg] = useState("sample message")
+  const [key, setKey] = useState({})
 
   const isCryptoAvailable = (typeof window.crypto.subtle !== 'undefined')
 
@@ -17,9 +21,9 @@ export default function Home() {
     return enc.encode(msg);
   }
 
-  async function generate() {
-    setKey('generating...')
-    let res = await window.crypto.subtle.generateKey(
+  const generate = async () => {
+    setStatus('generating...')
+    const res = await window.crypto.subtle.generateKey(
       {
         name: "RSA-OAEP",
         modulusLength: 4096,
@@ -29,59 +33,115 @@ export default function Home() {
       true,
       ["encrypt", "decrypt"],
     );
-
-    setKey('converting...')
+    setStatus('converting...')
     const publicKey = await window.crypto.subtle.exportKey("jwk", res.publicKey);
     const privateKey = await window.crypto.subtle.exportKey("jwk", res.privateKey);
-    setKey('done converting')
+    setKey({ publicKey, privateKey })
 
-    setKey(JSON.stringify({ publicKey, privateKey }))
-    await sleep(1000)
+    setStatus('done converting')
+  }
 
-    setKey('encrypting...')
+  const simulateEncryption = async () => {
+    const publicKey = await window.crypto.subtle.importKey(
+      "jwk",
+      key.publicKey,
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      true,
+      ["encrypt"],
+    );
+
+    const privateKey = await window.crypto.subtle.importKey(
+      "jwk",
+      key.privateKey,
+      {
+        name: "RSA-OAEP",
+        hash: "SHA-256",
+      },
+      true,
+      ["decrypt"],
+    );
+
+    setStatus('encrypting...')
+    let msg = `original Message: ${origMsg}`
 
     const cipherText = await window.crypto.subtle.encrypt(
         {
           name: "RSA-OAEP",
         },
-        res.publicKey,
-        encodeMessage('abcdefg'),
+        publicKey,
+        encodeMessage(origMsg)
       );
 
+    const decodedChiperText = window.btoa(cipherText)
     let decoder = new TextDecoder()
-    setKey('encryption result: ' + decoder.decode(cipherText))
+    msg += "\nencryption result: " + decoder.decode(cipherText)
 
-    await sleep(3000)
-
-    setKey('decrypting...')
+    setStatus('decrypting...')
     
     const decryptRes = await window.crypto.subtle.decrypt(
       { name: "RSA-OAEP" },
-      res.privateKey,
+      privateKey,
       cipherText,
     );
-    setKey('decryption result: ' + decoder.decode(decryptRes))
+    msg += "\ndecryption result: " + decoder.decode(decryptRes)
+
+    setEncryptionMessage(msg)
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>WhisperBox</h1>
+      <div className="p-10 bg-blue-200 w-full max-w-5xl items-center justify-between font-mono text-sm ">
+        <h1 class="text-xl">WhisperBox</h1>
 
-        <p>Share secrets safely, easily</p>
+        <p class="font-italic">Share secrets safely, easily</p>
+      </div>
 
+      <article className="p-10 mt-5 bg-blue-200">
         <p>crypto is { isCryptoAvailable ? 'AVAILABLE' : 'NOT AVAILABLE' }</p>
 
         <p>
-          key: { key }
+          status: { status }
         </p>
+
+        <pre class="bg-gray-200 p-4 overflow-x-scroll max-w-2xl">
+          key: { JSON.stringify(key, true, " ") }
+        </pre>
 
         <button
           type="button"
           onClick={generate}
           className="bg-blue-300 p-4 border rounded hover:bg-blue-500"
-        >generate key</button>
-      </div>
+        >Generate Keys</button>
+      </article>
+
+      <article className="p-10 mt-5 bg-blue-200">
+        <div>
+          <label>
+            Message: 
+          </label>
+          <input
+            type="text"
+            value={origMsg}
+            onInput={e => setOrigMsg(e.currentTarget.value)} 
+          />
+        </div>
+
+        <pre class="bg-gray-200 p-4 overflow-x-scroll max-w-2xl">
+          Encryption Message:
+          { encryptionMessage }
+        </pre>
+
+        <button
+          type="button"
+          onClick={simulateEncryption}
+          className="bg-blue-300 p-4 border rounded hover:bg-blue-500"
+        >
+          Simulate encryption
+        </button>
+      </article>
     </main>
   )
 }
